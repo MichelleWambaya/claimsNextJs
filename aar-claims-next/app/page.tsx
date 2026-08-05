21b31804eabcc27865afbfab7c90a4e5f5c8810e
Michelle Wambaya <michellewambaya@gmail.com>
2026-08-05 11:44:09 +0300

    deleted

--- aar-claims-next/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
-
-export default async function RootPage() {
-  const supabase = createClient();
-  const { data } = await supabase.auth.getUser();
-  redirect(data.user ? "/dashboard" : "/login");
-}
